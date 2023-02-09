effccb08f247ed4dad0337f78d2e492a65311f31
JairAFM <jair.martinez@galileo.edu>
2023-02-08 18:21:09 -0600

    jflores
    se sube timer

--- threads/thread.h
+++ threads/thread.h
@@ -93,6 +93,8 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    uint64_t timer_sleep               /* Entero que representa el tiempo que un thread debe permanecer dormido*/
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -137,5 +139,8 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+
+void insertar_en_lista_espera(int64_t ticks); /*Toma el thread actual, lo bloquea y lo inserta en la lista de espera (lista_espera)*/
+void remover_thread_durmiente(int64_t ticks); /*Desbloquea el thread cuando haya cumplido su tiempo de expiracion*/
 
 #endif /* threads/thread.h */
